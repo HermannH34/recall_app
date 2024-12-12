55976a27e56edccfc43a48d7da129268c4a31d3d
Hermann Hairet <hermannhairet44@gmail.com>
2024-12-12 23:33:59 +0100

    add mailer

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import connectMongo from "@/libs/mongoose";
 import Recall from "@/models/Recall";
+import { sendEmail } from "@/libs/resend";
 
 export async function GET() {
  try {
@@ -8,17 +9,16 @@ export async function GET() {
 
   const { startDate, endDate } = getDateRangeForToday();
 
-  const leads = await Recall.find({
+  const recalls = await Recall.find({
    recallDate: {
     $gte: startDate,
     $lt: endDate,
    },
   });
 
+  await sendEmailsForLeads(recalls);
 
-  console.log("Leads to recall:", leads);
-
-  return NextResponse.json({ success: true, leads });
+  return NextResponse.json({ success: true, recalls });
  } catch (error) {
   console.error("Error fetching leads by date: ", error);
   return NextResponse.json(
@@ -28,7 +28,6 @@ export async function GET() {
  }
 }
 
-
 const getDateRangeForToday = (): { startDate: Date; endDate: Date } => {
  const today = new Date();
  today.setHours(0, 0, 0, 0);
@@ -37,4 +36,46 @@ const getDateRangeForToday = (): { startDate: Date; endDate: Date } => {
  tomorrow.setDate(today.getDate() + 1);
 
  return { startDate: today, endDate: tomorrow };
+};
+
+const generateEmailBody = (recall: {
+ name: string;
+ motive: string;
+}): { text: string; html: string } => {
+ const text = `
+Nom: ${recall.name}
+Motif de rappel: ${recall.motive}
+
+`;
+
+ const html = `
+<p><strong>Nom:</strong> ${recall.name}</p>
+<p><strong>Motif de rappel:</strong> ${recall.motive}</p>
+<br>
+`;
+
+ return { text, html };
+};
+
+const sendEmailsForLeads = async (recalls: any[]) => {
+ for (const recall of recalls) {
+  const { text, html } = generateEmailBody({
+   name: recall.name,
+   motive: recall.motive,
+  });
+
+  try {
+   await sendEmail({
+    to: recall.email,
+    subject: "Rappel programmé",
+    text,
+    html,
+    replyTo: "hermannhairet44@gmail.com",
+   });
+
+   console.log(`Email sent to ${recall.email}`);
+  } catch (emailError) {
+   console.error(`Error sending email to ${recall.email}:`, emailError);
+  }
+ }
 };
