4fd7b32303b73f7c1df515b0d52989b69c78b028
Hermann Hairet <hermannhairet44@gmail.com>
2024-12-23 14:58:10 +0100

    add useTransition in RecallCheckbox

--- components/RecallCheckbox.tsx
+++ components/RecallCheckbox.tsx
@@ -1,19 +1,26 @@
-'use client'
+'use client';
 
-import { toggleRecall } from "@/actions/recalls"
+import { toggleRecall } from "@/actions/recalls";
+import { useTransition } from "react";
 
 interface RecallCheckboxProps {
   recallId: string;
 }
 
 export default function RecallCheckbox({ recallId }: RecallCheckboxProps) {
+  const [isPending, startTransition] = useTransition();
+
   const handleChange = async () => {
-    const result = await toggleRecall(recallId)
+    const result = await toggleRecall(recallId);
 
     if (!result.success) {
-      console.error('Failed to update recall status')
+      console.error('Failed to update recall status');
+    } else {
+      startTransition(() => {
+        window.location.reload();
+      });
     }
-  }
+  };
 
   return (
     <label>
@@ -21,7 +28,8 @@ export default function RecallCheckbox({ recallId }: RecallCheckboxProps) {
         type="checkbox" 
         className="checkbox checkbox-primary" 
         onChange={handleChange}
+        disabled={isPending}
       />
     </label>
-  )
-} 
+  );
+}
